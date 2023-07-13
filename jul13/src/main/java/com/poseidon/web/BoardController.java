bb6fc754107bd5aae31e4d40b139654580ac74c9
user <user@705Server>
2023-07-13 14:29:04 +0900

    dto vs map

--- jul13/src/main/java/com/poseidon/web/BoardController.java
+++ jul13/src/main/java/com/poseidon/web/BoardController.java
@@ -1,17 +1,52 @@
 package com.poseidon.web;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 // Ctrl + shift + o --> 임포트 정리해줘요.
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class BoardController {
-	
-	//사용자가 /board라고 호출하면 동작할 메소드를 만들겠습니다. 
-	
+
+	// 사용자가 /board라고 호출하면 동작할 메소드를 만들겠습니다.
 	@GetMapping("/board")
-	public String board() {
-		return "board"; //jsp파일명		
+	public ModelAndView board() {
+		ModelAndView mv = new ModelAndView("board");// jsp파일명
+		mv.addObject("name", "홍길동");
+
+		List<BoardDTO> list = new ArrayList<BoardDTO>();
+		for (int i = 1; i <= 10; i++) {
+			BoardDTO e = new BoardDTO(i, i + "번째 글 입니다", "홍길동", "2023-07-" + i, i * 10);
+			// e.setBno(i);
+			// e.setBtitle(i + "번째 글 입니다");
+			// e.setBwrite("홍길동");
+			// e.setBdate("2023-07-" + i);
+			// e.setBlike(i * 10);
+			list.add(e);
+		}
+
+		mv.addObject("list", list);
+
+		List<Map<String, Object>> list2 = new ArrayList<Map<String, Object>>();
+
+		for (int i = 1; i < 10; i++) {
+			Map<String, Object> e = new HashMap<String, Object>();
+			e.put("bno", i);
+			e.put("btitle", i + "번째 글 입니다2");
+			e.put("bwrite", "홍길동");
+			e.put("bdate", "2023-07-13");
+			e.put("blike", i * 10);
+			list2.add(e);
+		}
+
+		mv.addObject("list2", list2);
+
+		return mv;
 	}
 
 }
--- jul13/src/main/webapp/WEB-INF/views/board.jsp
+++ jul13/src/main/webapp/WEB-INF/views/board.jsp
@@ -1,13 +1,74 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>보드</title>
+<link href="./css/board.css" rel="stylesheet">
 </head>
 <body>
-	<h1>보드</h1>
-	데이터는 잠시 후에 붙여보겠습니다.
+	<h1>보드 : ${name }</h1>
+	데이터는 잠시 후에 붙여보겠습니다.<br>
+	<hr>
+	<table>
+		<thead>
+		<tr>
+			<th>번호</th>
+			<th>제목</th>
+			<th>글쓴이</th>
+			<th>날짜</th>
+			<th>조회수</th>
+		</tr>
+		</thead>
+		<tbody>
+		<c:forEach items="${list }" var="row">
+		<tr>
+			<td id="td1">${row.bno }</td>
+			<td class="title">${row.btitle }</td>
+			<td id="td1">${row.bwrite }</td>
+			<td id="td2">${row.bdate }</td>
+			<td id="td1">${row.blike }</td>
+		</tr>
+		</c:forEach>
+		</tbody>
+	</table>
+	
+	<hr>
+	<table>
+		<thead>
+		<tr>
+			<th>번호</th>
+			<th>제목</th>
+			<th>글쓴이</th>
+			<th>날짜</th>
+			<th>조회수</th>
+		</tr>
+		</thead>
+		<tbody>
+		<c:forEach items="${list2 }" var="row">
+		<tr>
+			<td id="td1">${row.bno }</td>
+			<td class="title">${row.btitle }</td>
+			<td id="td1">${row.bwrite }</td>
+			<td id="td2">${row.bdate }</td>
+			<td id="td1">${row.blike }</td>
+		</tr>
+		</c:forEach>
+		</tbody>
+	</table>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 </body>
 </html>
